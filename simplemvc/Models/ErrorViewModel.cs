3e07afd1401692142221509fce6d75271f211938
champaksworldcreate <champaksworld@gmail.com>
2023-06-29 20:53:40 +0530

    Initial commit

---
+++ simplemvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace simplemvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
